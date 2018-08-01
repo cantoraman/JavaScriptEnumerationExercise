1873896b12680e839cee81b9dec1e63b46550544
Can Toraman <cantoraman@gmail.com>
2018-08-01 11:58:44 +0100

    list of titles pass

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,9 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.listOfTitles = function () {
+  return this.films.map(film => film.title);
+}
+
+
 module.exports = Cinema;
